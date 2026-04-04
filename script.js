32eee0139ecb5721699aace0693b8eba2f6ecaf2
Vasto <sonicdarkultra44@gmail.com>
2026-04-04 17:36:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("full screen, resize");
+console.log("jolt bugged");
 
 // DODGE.IO - SCRIPT.JS
 const cnv = document.getElementById("game");
@@ -66,8 +66,8 @@ document.addEventListener("touchend", () => {
     const mouseInAbilityBtn = abilityOneBtn.contains(event.target) || abilityTwoBtn.contains(event.target);
     if (!mouseInAbilityBtn) {
         recordLeftClick();
+        allClicks.push(createClick("left"));
     }
-    allClicks.push(createClick("left"));
     mouseDown = false;
 });
 document.addEventListener("touchcancel", () => {
@@ -204,8 +204,15 @@ const abilityTwoBtn = document.getElementById("ability-two");
 
 const upEvents = ["touchend", "click"];
 upEvents.forEach((upEvent) => {
-    abilityOneBtn.addEventListener(upEvent, recordRightClick);
-    abilityTwoBtn.addEventListener(upEvent, recordMiddleClick);
+    abilityOneBtn.addEventListener(upEvent, () => {
+        recordRightClick();
+        allClicks.push(createClick("right"));
+    }
+                                   
+    abilityTwoBtn.addEventListener(upEvent, () => {
+        recordMiddleClick();
+        allClicks.push(createClick("middle"));
+    });
 })
 
 /* Ability Button hover and click effects */
@@ -226,10 +233,7 @@ window.addEventListener("load", () => {
                     }
                     else {
                         abilityBtn.style.opacity = 1;
-                        
-                        abilityBtn.style.backgroundColor = player.color;
-                        abilityBtn.style.borderColor = player.subColor;
-                        abilityBtn.style.color = player.subColor;
+                        colorAbilityButtons(abilityBtn);
                     }
                 });
             })
@@ -245,9 +249,7 @@ window.addEventListener("load", () => {
             
             upEvents.forEach((upEvent) => {
                 abilityBtn.addEventListener(upEvent, () => {
-                    abilityBtn.style.backgroundColor = player.color;
-                    abilityBtn.style.borderColor = player.subColor;
-                    abilityBtn.style.color = player.subColor;
+                    colorAbilityButtons(abilityBtn);
                 });
             })
         }
@@ -333,11 +335,7 @@ if (localData) {
         if (player.dodger === "quasar") { player.color = "rgb(255, 165, 0)"; player.subColor = "rgb(230, 153, 11)"; }
         highscore = userData.highscore;
 
-        [abilityOneBtn, abilityTwoBtn].forEach((abilityBtn) => {
-            abilityBtn.style.backgroundColor = player.color;
-            abilityBtn.style.borderColor = player.subColor;
-            abilityBtn.style.color = player.subColor;
-        })
+        [abilityOneBtn, abilityTwoBtn].forEach((abilityBtn) => colorAbilityButtons(abilityBtn));
         
         settings = userData.settings;
         musicVolume = Math.max(Math.min((settings.musicSliderX - 565) / (715 - 565), 1), 0);
